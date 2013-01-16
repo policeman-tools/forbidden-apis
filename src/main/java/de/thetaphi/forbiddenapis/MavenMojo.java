ef9880160d86a0135b64f2a1fbdd834d0fabae20
uwe.h.schindler@gmail.com <uwe.h.schindler@gmail.com@d792475c-d26d-5892-9e01-79372957b29b>
2013-01-16 11:14:53 +0000

    Use Java5 annotations for the Mojo, improve pom.xml and descriptor generator

--- src/main/java/de/thetaphi/forbiddenapis/MavenMojo.java
+++ src/main/java/de/thetaphi/forbiddenapis/MavenMojo.java
@@ -17,23 +17,23 @@ package de.thetaphi.forbiddenapis;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Echos an object string to the output screen.
- * @goal echo
- * @requiresProject false
  */
-public class Mojo extends AbstractMojo
-{
-    /**
-     * Any Object to print out.
-     * @parameter expression="${echo.message}" default-value="Hello World..."
-     */
-    private Object message;
+@Mojo(name = "echo", requiresProject = false, threadSafe = true)
+public class MavenMojo extends AbstractMojo {
 
-    public void execute()
-        throws MojoExecutionException, MojoFailureException
-    {
-        getLog().info( message.toString() );
-    }
+  /**
+   * Any Object to print out.
+   */
+  @Parameter(property = "echo.message", defaultValue = "Hello World...", required = true)
+  private Object message;
+
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().info(message.toString());
+  }
+  
 }
