fcae5a7ad414768b17b6a06192e8234f8cadd0b3
uschindler <uwe@thetaphi.de>
2014-11-24 00:27:38 +0000

    Tag release

---
+++ tags/1.7/src/main/docs/xdoc2html.xsl
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+ * (C) Copyright 2013 Uwe Schindler (Generics Policeman) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+-->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xdoc="http://maven.apache.org/XDOC/2.0" exclude-result-prefixes="xdoc">
+  <xsl:output method="html" encoding="UTF-8" media-type="text/html"/>
+  
+  <xsl:template match="/xdoc:document">
+    <html>
+      <head>
+        <title><xsl:value-of select="xdoc:properties/xdoc:title"/></title>
+        <style type="text/css"><![CDATA[
+          table {
+            border-collapse: collapse;
+          }
+          table, tr, th, td {
+            border: 1px solid dimgray;
+            vertical-align: top;
+            text-align: left;
+          }
+          th, td {
+            padding: .2em;
+          }
+        ]]></style>
+      </head>
+      <body>
+        <xsl:apply-templates select="xdoc:body/node()"/>
+      </body>
+    </html>
+  </xsl:template>
+	
+  <xsl:template match="xdoc:section">
+    <h1><xsl:value-of select="@name"/></h1>
+    <xsl:apply-templates/>
+  </xsl:template>
+	
+  <xsl:template match="xdoc:subsection">
+    <h2><xsl:value-of select="@name"/></h2>
+    <xsl:apply-templates/>
+  </xsl:template>
+	
+  <!-- template to copy elements -->
+  <xsl:template match="*">
+    <xsl:element name="{local-name()}">
+      <xsl:apply-templates select="@* | node()"/>
+    </xsl:element>
+  </xsl:template>
+
+  <!-- template to copy attributes -->
+  <xsl:template match="@*">
+    <xsl:attribute name="{local-name()}">
+      <xsl:value-of select="."/>
+    </xsl:attribute>
+  </xsl:template>
+
+  <!-- template to copy the rest of the nodes -->
+  <xsl:template match="comment() | text() | processing-instruction()">
+    <xsl:copy/>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ tags/1.7/src/main/java/de/thetaphi/forbiddenapis/TestCheckMojo.java
@@ -0,0 +1,74 @@
+package de.thetaphi.forbiddenapis;
+
+/*
+ * (C) Copyright 2013 Uwe Schindler (Generics Policeman) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Mojo to check if no project generated class files (test scope) contain calls to forbidden APIs
+ * from the project classpath and a list of API signatures (either inline or as pointer to files or bundled signatures).
+ * At least one signature must be given, using any of the corresponding optional parameters.
+ * @since 1.2
+ */
+@Mojo(name = "testCheck", threadSafe = true, requiresProject = true, requiresDependencyResolution = ResolutionScope.TEST, defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES)
+public final class TestCheckMojo extends AbstractCheckMojo {
+
+  /**
+   * Injected test classpath.
+   */
+  @Parameter(defaultValue = "${project.testClasspathElements}", readonly = true, required = true)
+  private List<String> classpathElements;
+
+  /**
+   * Directory with the class files to check.
+   */
+  @Parameter(required = false, defaultValue = "${project.build.testOutputDirectory}")
+  private File classesDirectory;
+  
+  /**
+   * The default compiler target version used to expand references to bundled JDK signatures.
+   * This setting falls back to "targetVersion" if undefined. This can be used to override
+   * the target version solely used for tests.
+   * E.g., if you use "jdk-deprecated", it will expand to this version.
+   * This setting should be identical to the target version used in the compiler plugin.
+   * @since 1.4
+   */
+  @Parameter(required = false, defaultValue = "${maven.compiler.testTarget}")
+  private String testTargetVersion;
+
+  @Override
+  protected List<String> getClassPathElements() {
+    return this.classpathElements;
+  }
+  
+  @Override
+  protected File getClassesDirectory() {
+    return this.classesDirectory;
+  }
+  
+  @Override
+  protected String getTargetVersion() {
+    return (testTargetVersion != null) ? testTargetVersion : super.getTargetVersion();
+  }
+  
+}
