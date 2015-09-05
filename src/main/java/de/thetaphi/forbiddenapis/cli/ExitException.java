ac2c1c0b76da39cb11c8d91f10eb5fd9ba738de1
Uwe Schindler <uwe@thetaphi.de>
2015-09-05 11:33:31 +0200

    Hide some exception classes which do not need to be public (internal only)

--- src/main/java/de/thetaphi/forbiddenapis/cli/ExitException.java
+++ src/main/java/de/thetaphi/forbiddenapis/cli/ExitException.java
@@ -4,7 +4,7 @@ package de.thetaphi.forbiddenapis.cli;
  * Used by the CLI to signal process exit with a specific exit code
  */
 @SuppressWarnings("serial")
-public final class ExitException extends Exception {
+final class ExitException extends Exception {
   public final int exitCode;
   
   public ExitException(int exitCode) {
