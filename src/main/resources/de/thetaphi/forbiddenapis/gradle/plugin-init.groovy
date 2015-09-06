c6bed964ac6380e5f9c3703d6dbc77ce0aefcad5
Uwe Schindler <uwe@thetaphi.de>
2015-09-06 21:10:43 +0200

    More code cleanup in task-init.groovy

--- src/main/resources/de/thetaphi/forbiddenapis/gradle/plugin-init.groovy
+++ src/main/resources/de/thetaphi/forbiddenapis/gradle/plugin-init.groovy
@@ -1,7 +1,5 @@
 // initializes the plugin and binds it to the lifecycle
 
-import org.gradle.api.Task;
-import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.plugins.PluginInstantiationException;
 import org.gradle.api.tasks.TaskContainer;
 
@@ -9,13 +7,10 @@ if (!project.plugins.findPlugin("java")) {
   throw new PluginInstantiationException("Forbiddenapis only works in projects using the 'java' plugin.");
 }
 
-ConfigurationContainer configurations = project.getConfigurations();
-TaskContainer tasks = project.getTasks();
+def tasks = project.getTasks();
 
-def forbiddenTasks = [];
-
-project.sourceSets.each { sourceSet ->
-  forbiddenTasks += tasks.create(sourceSet.getTaskName(plugin.FORBIDDEN_APIS_TASK_NAME_VERB, null), CheckForbiddenApis.class) { task ->
+def forbiddenTasks = project.sourceSets.collect { sourceSet ->
+  tasks.create(sourceSet.getTaskName(plugin.FORBIDDEN_APIS_TASK_NAME_VERB, null), CheckForbiddenApis.class) { task ->
     task.setClassesDir(sourceSet.output.classesDir);
     task.setClasspath(sourceSet.compileClasspath);
     task.dependsOn(sourceSet.output);
