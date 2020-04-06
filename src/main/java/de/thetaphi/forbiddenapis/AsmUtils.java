b78f65464389d20fbd0f4d3edeabf00a9f1713c7
Uwe Schindler <uwe@thetaphi.de>
2020-04-06 11:14:54 +0200

    Initial support for Java 14

--- src/main/java/de/thetaphi/forbiddenapis/AsmUtils.java
+++ src/main/java/de/thetaphi/forbiddenapis/AsmUtils.java
@@ -179,7 +179,7 @@ public final class AsmUtils {
   @SuppressWarnings("unused")
   public static ClassReader readAndPatchClass(InputStream in) throws IOException {
     final byte[] bytecode = readStream(in);
-    if (false) patchClassMajorVersion(bytecode, Opcodes.V10 + 1, Opcodes.V10);
+    if (false) patchClassMajorVersion(bytecode, Opcodes.V15 + 1, Opcodes.V15);
     return new ClassReader(bytecode);
   }
 
